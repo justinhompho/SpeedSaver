206d9ce1cbf5b8b6458f56a2350d49f65dff7b7d
Justin Ubuntu <justin.hompho@jmax-surf>
2024-10-01 15:27:34 -0500

    geolocation request check on mobile

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
 
-// const speedInput = document.querySelector("#js-speed-limit");
-// const speedLimit = document.getElementById("jsSpeedLimit");
-// speedLimit.placeholder = speedInput.value;
-// speedInput.addEventListener("input", (event) => {
-//   speedLimit.placeholder = event.target.value;
-// });
+// Geo location demo
+function geoFindMe() {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
+
+  mapLink.href = "";
+  mapLink.textContent = "";
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "";
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+document.querySelector("#find-me").addEventListener("click", geoFindMe);
+
 
 
 function calculateSpeed() {
 
   resetTable();
-  console.log("hello")
   var speedLimit = Number(document.querySelector('.js-speed-limit').value);
-  console.log("hello2")
   var mpg = Number(document.querySelector('.js-mpg').value);
   var distance = Number(document.querySelector('.js-distance').value);
   var gasPrice = Number(document.querySelector('.js-gas-price').value);
